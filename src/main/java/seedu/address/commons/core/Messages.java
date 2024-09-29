d523a7f2004892e46c1941081df21860685eafaa
Justincjr <jchoojr@hotmail.com>
2024-09-29 20:08:08 +0800

    Edit code for checkstyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -1,5 +1,8 @@
 package seedu.address.commons.core;
 
+/**
+ * User-visible messages.
+ */
 public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
